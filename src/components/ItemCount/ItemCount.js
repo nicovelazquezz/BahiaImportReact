90e99c7dbfb668e40e8468770046515670ee982e
nicc <nv_velazquez@hotmail.com>
2023-02-15 02:49:40 -0300

    logica quantity

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -10,14 +10,32 @@ function ItemCount({ stock, product }) {
 
   const [ goToCart, setGoToCart ]= useState(false)
 
-  const { quantity, setCart, setCount } = useContext(CartContext);
+  const { quantity, setCart, setCount, cart, count } = useContext(CartContext);
+
   
+  const agregarProductoAlCarrito = (product, cart, setCart) => {
+    // Verificar si el producto ya existe en el carrito
+    const productoExistente = cart.find((prod) => prod.id === product.id);
   
-  const onAdd = () => {    
-    setCart(prev => [...prev, {name, price, id, quantity, img, category}])    
-    setCount(1)   
+    if (productoExistente) {
+      // Si el producto ya está en el carrito, actualizar su cantidad
+      const newCart = cart.map((prod) => {
+        if (prod.id === product.id) {
+          return { ...prod, quantity: prod.quantity + count };
+        } else {
+          return prod;
+        }
+      });
+  
+      setCart(newCart);
+    } else {
+      // Si el producto no está en el carrito, agregarlo
+      setCart(prev => [...prev, {name, price, id, quantity, img, category}])    
+      setCount(1)  
+    }
   }
   
+
   
   const handleFinish = () => {
     console.log('si')
@@ -33,7 +51,7 @@ function ItemCount({ stock, product }) {
               <Counter 
                 stock={stock}        
               />
-              <button onClick={ () => {setGoToCart(true); onAdd()} } className='py-4 text-sm font-bold text-white uppercase bg-violet-500 rounded-sm px-14 hover:bg-violet-700'>
+              <button onClick={ () => {setGoToCart(true); agregarProductoAlCarrito(product, cart, setCart)} } className='py-4 text-sm font-bold text-white uppercase bg-violet-500 rounded-sm px-14 hover:bg-violet-700'>
                   Agregar al carrito
               </button>    
             </>
