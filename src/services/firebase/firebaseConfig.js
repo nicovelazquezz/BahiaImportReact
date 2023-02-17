18070926975305eca90dc7d161dd5937fdf6533d
nicc <nv_velazquez@hotmail.com>
2023-02-17 13:10:42 -0300

    env firebaseconfig

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAPYtNUvQgiwbi67pKdLVku2RpRnYBSId0",
-    authDomain: "backend34860-a5d84.firebaseapp.com",
-    projectId: "backend34860-a5d84",
-    storageBucket: "backend34860-a5d84.appspot.com",
-    messagingSenderId: "126784833911",
-    appId: "1:126784833911:web:60a8cc069d52d20021f972"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
