3db188140042c846d252d78eb76df1b5af7e2b0d
nicc <nv_velazquez@hotmail.com>
2024-03-20 00:03:58 -0300

    test

--- src/App.js
+++ src/App.js
@@ -3,34 +3,22 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/NavBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import Cart from './components/Cart/Cart'
-import { CartProvider } from './context/CartContext';
-import Checkout from './components/Checkout/Checkout'
-import { ToastContainer } from 'react-toastify';
+
 
 function App() {
-
-
-
   return (
-    <>     
-      <CartProvider>
+    <div>
         <BrowserRouter>
           <NavBar />
           <Routes>
-            <Route path='/' element={ <ItemListContainer greeting='Bienvenidos a Bahia Import'/>  } />
-            <Route path='/category/:categoryId' element={ <ItemListContainer greeting='Bienvenidos a Bahia Import'/>  } />
-            <Route path='/item/:productId' element={ <ItemDetailContainer /> } />
-            <Route path='/cart' element={ <Cart /> } />
-            <Route path='/checkout' element= { <Checkout /> } />
+            <Route path='/' element={ <ItemListContainer greeting='Bienvenidos al himalaya'/>  } />
+            <Route path='/detail/:productId' element={ <ItemDetailContainer /> } />
           </Routes>
         </BrowserRouter>
-        </CartProvider>      
-        <ToastContainer newestOnTop />
-    </>   
-    
+
+      
+    </div>
   );
 }
 
 export default App;
-
